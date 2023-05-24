a70b6478e35b0fc52cbe193a6ade534d70574cb3
Thomas-jayaruban <cathomasjayaruban@gmail.com>
2023-05-24 15:31:51 +0530

    first commit

---
+++ src/app/mainpage/mainpage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage',
+  templateUrl: './mainpage.component.html',
+  styleUrls: ['./mainpage.component.css']
+})
+export class MainpageComponent {
+  hideValue:boolean = false;
+  addTask(){
+   
+    this.hideValue = true;
+  }
+  
+}
